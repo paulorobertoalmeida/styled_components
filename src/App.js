5af49ab8eb7ff512680787bd0042ba3aeee0cdf6
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-12-02 11:35:11 +0100

    feat: First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
+import styled from 'styled-components'
 import './App.css';
-
+import {Wrapper, Title } from './components/globalcomponents'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Title>this is the app to test Styled Components</Title>
+      <h1>Hello Moto</h1>
+      </Wrapper>
   );
 }
 
